66167bfb98bcf358cff800f90b9b66fa9c1e79f6
Henry Chen <tahafut@gmail.com>
2015-08-06 18:20:16 -0700

    add Hoare partition

--- quicksort.py
+++ quicksort.py
@@ -6,19 +6,38 @@ def _qsort(array, start, end):
     if end - start < 1:
         return
     else:
-        pivot = partition(array, start, end)
+        pivot = partition_hoare(array, start, end)
         _qsort(array, start, pivot - 1)
         _qsort(array, pivot + 1, end)
 
 
-def partition(array, start, end):
-    # print array
+def partition_lomuto(array, start, end):
     pivot = start  # pivot position to swap with pivot value; first value to the right
+
     for i in range(start, end):  # iterate from start to end - 1
         if array[i] <= array[end]:  # current value <= pivot value
             array[i], array[pivot] = array[pivot], array[i]  # swap current and value at pivot position
             pivot += 1  # update pivot position
-            # print array
+
     array[end], array[pivot] = array[pivot], array[end]  # swap pivot value and value at pivot position
-    # print array
     return pivot
+
+
+def partition_hoare(array, start, end):
+    i, j = start, end - 1
+
+    while True:
+        while array[i] <= array[end] and i < j:
+            i += 1
+
+        while array[j] >= array[end] and i < j:
+            j -= 1
+
+        if i == j:
+            if array[i] <= array[end]:
+                i += 1
+
+            array[i], array[end] = array[end], array[i]
+            return i
+        else:
+            array[i], array[j] = array[j], array[i]
